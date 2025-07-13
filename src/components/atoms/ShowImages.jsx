680d5745f708dc7f0f57124421160822f30e90ff
Sayhan Ahmed <sayhan@ewnbd.com>
2025-07-14 00:11:37 +0600

    Show image desing changed

--- src/components/atoms/ShowImages.jsx
+++ src/components/atoms/ShowImages.jsx
@@ -14,15 +14,15 @@ const ShowImages = () => {
     <div className="h-fit w-full">
       <SecureImage
         image={image}
-        className="h-full max-h-[600px] min-h-[550px] w-full rounded-2xl object-cover"
+        className="h-full min-h-[300px] w-full rounded-2xl object-cover sm:max-h-[600px] sm:min-h-[550px]"
       />
-      <div className="mt-3 flex max-h-[100px] gap-3">
+      <div className="mt-3 flex max-h-[100px] flex-wrap gap-1 sm:gap-3">
         {[carImage, carImage2, carImage3, carImage4].map((img) => (
           <Button key={img} onClick={() => setImage(img)} className="m-0 p-0">
             <SecureImage
               key={img}
               image={img}
-              className="h-full max-h-[70px] min-h-[70px] w-[120px] rounded-md object-cover"
+              className="h-full max-h-[70px] min-h-[30px] min-w-[40px] rounded-md object-cover sm:min-h-[70px] sm:w-[120px]"
             />
           </Button>
         ))}
